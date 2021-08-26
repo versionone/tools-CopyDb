3656870df9fa76636c46f84d98a00e9aa8c89723
rajiv <rajiv.delwadia@versionone.com>
2021-08-26 18:53:57 -0400

    fix unreliable computed column definitions

--- TableSchemaInfo.sql
+++ TableSchemaInfo.sql
@@ -15,17 +15,17 @@ SELECT
 		WHEN DATA_TYPE IN ('decimal','numeric')
 			THEN cast(NUMERIC_SCALE as int)
 	END,
-	IsNullable = CASE Upper(IS_NULLABLE) WHEN 'NO' THEN cast(0 as bit) ELSE cast(1 as bit) END,
+	IsNullable = CASE Upper(COLUMNS.IS_NULLABLE) WHEN 'NO' THEN cast(0 as bit) ELSE cast(1 as bit) END,
 	IsIdentity = CASE COLUMNPROPERTY(OBJECT_ID(TABLE_NAME), COLUMN_NAME, 'IsIdentity') WHEN 1 THEN cast(1 as bit) ELSE cast(0 as bit) END,
 	IdentitySeed = CASE COLUMNPROPERTY(OBJECT_ID(TABLE_NAME), COLUMN_NAME, 'IsIdentity') WHEN 1 THEN cast(IDENT_SEED(TABLE_NAME) as int) END,
 	IdentityIncrement = CASE COLUMNPROPERTY(OBJECT_ID(TABLE_NAME), COLUMN_NAME, 'IsIdentity') WHEN 1 THEN cast(IDENT_INCR(TABLE_NAME) as int) END,
-	Calculation = com.text,
+	Calculation = com.definition,
 	cast(ORDINAL_POSITION as int) Position,
-	Collation = COLLATION_NAME
+	Collation = COLUMNS.COLLATION_NAME
 FROM INFORMATION_SCHEMA.COLUMNS
 JOIN sys.objects o on o.name=TABLE_NAME and o.schema_id=SCHEMA_ID(TABLE_SCHEMA)
 left join sys.extended_properties e on class=1 and e.major_id=o.object_id and e.minor_id=0 and e.name=N'microsoft_database_tools_support'
-LEFT JOIN syscomments com on com.id=OBJECT_ID(TABLE_NAME) AND com.number=ORDINAL_POSITION
+LEFT JOIN sys.computed_columns com on com.object_id=OBJECT_ID(TABLE_NAME) AND com.name=COLUMN_NAME
 where o.type='U' AND ObjectProperty(o.object_id, N'IsMSShipped')=0 AND (o.parent_object_id=0 OR ObjectProperty(o.parent_object_id, N'IsMSShipped')=0)
 	and isnull(e.value, 0)<>1
 --where TABLE_NAME=@tablename
